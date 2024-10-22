1e3e880179f0f34444f692f8739136ff99b7aad1
ajuuz <ajuuz07@gmail.com>
2024-10-22 13:55:03 +0530

    add routes and create pages

--- Frontend/React-with-Backend/src/App.jsx
+++ Frontend/React-with-Backend/src/App.jsx
@@ -6,13 +6,22 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 // components
 import AuthComponenet from './Components/AuthComponents/AuthComponet';
 
+// pages
+import Home from './Pages/Home/Home';
+import About from './Pages/About/About';
+import UserDashboard from './Pages/Dashboards/UserDashboard/UserDashboard';
+
+
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='about' element={<About/>}/>
         <Route path='auth/user/login' element={<AuthComponenet auth="USER LOGIN"/>}/>
         <Route path='auth/user/signup' element={<AuthComponenet auth="USER SIGNUP"/>}/>
         <Route path='auth/admin/login' element={<AuthComponenet auth="ADMIN LOGIN"/>}/>
+        <Route path='user/dashboard' element={<UserDashboard/>}/>
       </Routes>
     </Router>
   )
